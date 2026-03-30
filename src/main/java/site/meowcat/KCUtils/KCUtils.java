c88473fa9e5910dabe7970a5d72f38722662c42d
Ben <meowcat@meowcat.site>
2026-03-30 14:08:37 +0100

    :fishsticks: gitignore

---
+++ src/main/java/site/meowcat/KCUtils/KCUtils.java
@@ -0,0 +1,17 @@
+package site.meowcat.KCUtils;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KCUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
