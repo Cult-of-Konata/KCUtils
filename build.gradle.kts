1eac4f35033a763c634004675897659276faf206
Ben <meowcat@meowcat.site>
2026-03-31 21:58:04 +0100

    getting ready for auto publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-library")
     id("xyz.jpenilla.run-paper") version "3.0.2"
     id("com.gradleup.shadow") version "9.3.1"
+    id("io.papermc.hangar-publish-plugin") version "0.1.2"
 }
 
 version = "1.0.0"
