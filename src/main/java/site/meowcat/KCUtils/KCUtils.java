be57b4103b12e448f5f484a2a3c178049b612f31
Ben <meowcat@meowcat.site>
2026-04-01 09:42:17 +0100

    generate a pi chart based on most used language

--- src/main/java/site/meowcat/KCUtils/KCUtils.java
+++ src/main/java/site/meowcat/KCUtils/KCUtils.java
@@ -2,6 +2,7 @@ package site.meowcat.KCUtils;
 
 import net.kyori.adventure.text.Component;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.java.JavaPlugin;
 import site.meowcat.KCUtils.commands.*;
 
@@ -12,12 +13,19 @@ public final class KCUtils extends JavaPlugin {
     public void onEnable() {
         // Note that all commands that need WarningManager have "warningManager" as a parameter.
         WarningManager warningManager = new WarningManager(this);
-        Metrics metrics = new Metrics(this, 30511);
+        createCharts();
         this.getCommand("about").setExecutor(new AboutCommand(this));
         this.getCommand("ban-ip").setExecutor(new BanIpCommand(this));
         this.getCommand("warn").setExecutor(new WarnCommand(warningManager));
         this.getCommand("warnings").setExecutor(new WarningsCommand(warningManager));
         this.getCommand("warnclear").setExecutor(new WarnClearCommand(warningManager));
+    }
+
+    public void createCharts() {
+        Metrics metrics = new Metrics(this, 30511);
+        metrics.addCustomChart(new SimplePie("used_language", () -> {
+            return getConfig().getString("language", "English");
+        }));
     }
 
     @Override
