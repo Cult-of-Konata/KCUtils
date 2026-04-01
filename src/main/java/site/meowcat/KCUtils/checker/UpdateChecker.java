e9f5ed0ebfb5b23e96f0c69cc4ee9be9d79aeec2
Ben <meowcat@meowcat.site>
2026-04-01 22:22:05 +0100

    refactor updatechecker

--- src/main/java/site/meowcat/KCUtils/checker/UpdateChecker.java
+++ src/main/java/site/meowcat/KCUtils/checker/UpdateChecker.java
@@ -1,15 +1,19 @@
 package site.meowcat.KCUtils.checker;
 
-import org.apache.commons.lang3.concurrent.BackgroundInitializer;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonParser;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 public class UpdateChecker {
     private final JavaPlugin plugin;
+    private boolean updateAvailable = false;
+    private String latestVersion;
 
     public UpdateChecker(JavaPlugin plugin) {
         this.plugin = plugin;
@@ -18,25 +22,34 @@ public class UpdateChecker {
     public void check() {
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             try {
-                String current = plugin.getPluginMeta().getVersion();
+                String currentVersion = plugin.getPluginMeta().getVersion();
 
                 URL url = new URL(
                         "https://hangar.papermc.io/api/v1/projects/Cult-of-Konata/KCUtils/versions"
                 );
 
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(url.openStream())
-                );
+                InputStreamReader reader = new InputStreamReader(url.openStream());
+                JsonArray versions = new JsonParser().parse(reader).getAsJsonArray();
+                JsonObject newest = versions.get(0).getAsJsonObject();
+                latestVersion = newest.get("version").getAsString();
 
-                String reponse = reader.readLine();
-
-                if (!reponse.contains(current)) {
-                    plugin.getLogger().info("A new version of KCUtils is available: " + reponse);
-                    plugin.getLogger().info("Download it at: https://hangar.papermc.io/Cult-of-Konata/KCUtils");
+                if (!currentVersion.equalsIgnoreCase(latestVersion)) {
+                    updateAvailable = true;
+                    plugin.getLogger().info("A new update is available! Current version: " + currentVersion + ", Latest version: " + latestVersion);
+                    plugin.getLogger().info("https://hangar.papermc.io/Cult-of-Konata/KCUtils" + latestVersion);
+                } else {
+                    plugin.getLogger().info("You are running the latest version of KCUtils.");
                 }
             } catch (Exception e) {
-                plugin.getLogger().info("Failed to check for updates.");
+               plugin.getLogger().info("An error occurred while checking for updates.");
             }
         });
+    }
+
+    public boolean isUpdateAvailable() {
+        return updateAvailable;
+    }
+    public String getLatestVersion() {
+        return latestVersion;
     }
 }
