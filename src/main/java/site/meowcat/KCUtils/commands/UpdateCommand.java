2fa4e7cb8fd1d6eb7404c07ce4c36690abdb4f29
Ben <meowcat@meowcat.site>
2026-04-02 10:43:47 +0100

    add command to check for update

--- src/main/java/site/meowcat/KCUtils/commands/UpdateCommand.java
+++ src/main/java/site/meowcat/KCUtils/commands/UpdateCommand.java
@@ -1,4 +1,26 @@
 package site.meowcat.KCUtils.commands;
 
-public class UpdateCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
+import site.meowcat.KCUtils.checker.UpdateChecker;
+
+public class UpdateCommand implements CommandExecutor {
+    private final UpdateChecker checker;
+
+    public UpdateCommand(UpdateChecker checker) {
+        this.checker = checker;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NonNull @NotNull String[] args) {
+        if (!sender.isOp()) {
+            sender.sendMessage("§7 You must be a server operator to use this command!");
+        }
+        checker.check();
+        return true;
+
+    }
 }
